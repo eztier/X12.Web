57310741ec7149d055bab7bd9d659e95bdfcef8c
once-ler <htao@eztier.com>
2017-06-07 15:17:51 -0400

    Baseline

---
+++ test/transform-to-x12.sh
@@ -0,0 +1,1 @@
+curl -XPOST -H 'Content-Type: application/x-www-form-urlencode' --data-binary @C:/Users/taoh02/Dropbox/Apps/X12.Web/test/fixtures/837/5010/UnicodeExample.txt.xml http://localhost:62276/Hippa.svc/transform/xml
