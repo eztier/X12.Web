ecc48859146d1e2e1ddfa9dd522b64eafab022a7
once-ler <htao@eztier.com>
2017-06-09 12:15:46 -0400

    Main project is lib

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("b62eaf21-a9e9-44bb-8c91-f6f3046ada4d")]
+[assembly: Guid("39f73b89-e360-454c-820e-2b90165c1638")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -29,7 +29,7 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Revision and Build Numbers 
+// You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
