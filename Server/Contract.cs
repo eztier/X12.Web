3d179c1296c0623b34bf82625220c95d4c232c80
once-ler <htao@eztier.com>
2017-06-09 11:45:58 -0400

    Create separate project for server

---
+++ Server/Contract.cs
@@ -0,0 +1,26 @@
+﻿using System.Runtime.Serialization;
+using System.Collections.Generic;
+using System.ComponentModel;
+
+namespace X12.Web {
+  /*!
+   * Contracts
+   */
+  [DataContract(Namespace = "")]
+  public class DataManagementRequest {
+    [DataMember]
+    public string requestXmlString {
+      get;
+      set;
+    }
+  }
+
+  [DataContract(Namespace = "")]
+  public class DataManagementResponse {
+    [DataMember]
+    public string response {
+      get;
+      set;
+    }
+  }
+}
---
+++ Server/TransformToX12.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace X12.Web {
+  public partial class Hippa : IHippa {
+    public Stream TransformToX12(Stream postData) {
+      string resp = string.Empty;
+      var parser = new OopFactory.X12.Parsing.X12Parser();
+
+      try {
+        var xmltext = new StreamReader(postData, Encoding.UTF8).ReadToEnd();
+        resp = parser.TransformToX12(xmltext);
+      } catch(Exception e) {
+        WebOperationContext.Current.OutgoingResponse.StatusCode = System.Net.HttpStatusCode.BadRequest;
+        resp = e.Message;
+      }
+
+      WebOperationContext.Current.OutgoingResponse.ContentType = "text/plain";
+      Stream stream = new MemoryStream(System.Text.Encoding.UTF8.GetBytes(resp));
+      OperationContext clientContext = OperationContext.Current;
+
+      clientContext.OperationCompleted += new EventHandler((o, s) => {
+        if (stream != null)
+          stream.Dispose();
+      });
+
+      return stream;
+    }
+  }
+}
