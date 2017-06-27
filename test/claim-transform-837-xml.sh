5d0ff00c20126be65c26505f89882c414144bd6f
once-ler <htao@eztier.com>
2017-06-27 18:20:09 -0400

    Add local references to OopFactory.X12

--- test/claim-transform-837-xml.sh
+++ test/claim-transform-837-xml.sh
@@ -1,6 +1,6 @@
-curl -XPOST -H 'Content-Type: application/x-www-form-urlencode' --data-binary @/home/htao/X12.Web/test/fixtures/Claims/InstitutionalClaim4010.txt http://localhost:8169/Hippa/claim/transform/837/xml
+curl -XPOST -H 'Content-Type: application/x-www-form-urlencode' --data-binary @/home/htao/X12.Web/test/fixtures/Claims/InstitutionalClaim4010.txt http://localhost:8169/Hippa/claims/transform/837/xml
 
-curl -XPOST -H 'Content-Type: application/x-www-form-urlencode' --data-binary @/home/htao/X12.Web/test/fixtures/Claims/InstitutionalClaim5010.txt http://localhost:8169/Hippa/claim/transform/837/xml
+curl -XPOST -H 'Content-Type: application/x-www-form-urlencode' --data-binary @/home/htao/X12.Web/test/fixtures/Claims/InstitutionalClaim5010.txt http://localhost:8169/Hippa/claims/transform/837/xml
 
-curl -XPOST -H 'Content-Type: application/x-www-form-urlencode' --data-binary @/home/htao/X12.Web/test/fixtures/Claims/ProfessionalClaim1.txt http://localhost:8169/Hippa/claim/transform/837/xml
+curl -XPOST -H 'Content-Type: application/x-www-form-urlencode' --data-binary @/home/htao/X12.Web/test/fixtures/Claims/ProfessionalClaim1.txt http://localhost:8169/Hippa/claims/transform/837/xml
 
